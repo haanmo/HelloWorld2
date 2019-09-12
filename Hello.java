e1bfcf94b16bde89ed497515df45510d62776361
Haan Mo Johng <haanmo@naver.com>
2019-09-12 10:10:41 -0500

    change test

---
+++ Hello.java
@@ -0,0 +1,12 @@
+// test 2
+public class Hello{
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Hello World ...!!!!!! 091219_2" + i);
+		}
+		test();
+	}
+	void test(){
+		System.out.println("test1");
+	}
+}
