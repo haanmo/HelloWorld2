f826ff05ba4f39b29194f5655fda607fb5165d3e
Haan Mo Johng <haanmo@naver.com>
2019-09-12 10:23:51 -0500

    change test

--- Hello.java
+++ Hello.java
@@ -4,9 +4,5 @@ public class Hello{
 		for (int i = 1; i <= 5; i++) {
 			System.out.println("Hello World ...!!!!!! 091219_2" + i);
 		}
-		test();
-	}
-	void test(){
-		System.out.println("test1");
 	}
 }
